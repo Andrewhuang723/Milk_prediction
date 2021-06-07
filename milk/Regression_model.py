e81495105b6c9b3bf0fd31cb1791a92bf63f1bc4
Andrewhuang723 <68266640+Andrewhuang723@users.noreply.github.com>
2021-06-07 21:07:00 +0800

    Update Regression_model.py

--- milk/Regression_model.py
+++ milk/Regression_model.py
@@ -1,7 +1,9 @@
 import torch.nn as nn
 import torch
 from torch.utils.data import Dataset, DataLoader
-
+import numpy as np
+from time import time
+from milk.pytorch_tools import EarlyStopping
 
 class Model(nn.Module):
     def __init__(self, n_features, n_hidden):
@@ -31,3 +33,63 @@ class Data(Dataset):
 
     def __getitem__(self, item):
         return self.X[item], self.y[item]
+
+
+early_stopping = EarlyStopping(patience=10)
+
+def train(model, epochs, loss_func, data_loader, optimizer,  val_data_loader=None):
+    train_epochs_losses = []
+    val_epoch_losses = []
+    dur = []
+
+
+    for epoch in range(epochs):
+        model.train()
+        train_epochs_loss = 0
+        if epoch >= 1:
+            t0 = time()
+        for X, y in data_loader:
+            X = X.cuda()
+            y = y.cuda()
+            y = y.view(-1, 1)
+            y_pred = model(X)
+            loss = 0
+            for i in range(len(y)):
+                loss += loss_func(y_pred[i, :], y[i, :])
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_epochs_loss += (loss / len(X))
+        train_epochs_loss /= len(data_loader)
+
+        val_epoch_loss = 0
+        model.eval()
+        for X_val, y_val in val_data_loader:
+            X_val = X_val.cuda()
+            y_val = y_val.cuda()
+            y_val = y_val.view(-1, 1)
+            y_val_pred = model(X_val)
+            val_loss = 0
+            for i in range(len(y_val)):
+                val_loss += loss_func(y_val_pred[i, :], y_val[i, :])
+            val_epoch_loss += (val_loss / len(y_val))
+        val_epoch_loss /= len(val_data_loader)
+
+        if epoch >= 1:
+            dur.append(time() - t0)
+
+        early_stopping(val_loss=val_epoch_loss, model=model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+        print('Epoch {} | loss {:.6f} | Time(s) {:.4f} | val_loss {:.6f}'.format(epoch, train_epochs_loss,
+                                                                                 np.mean(dur),
+                                                                                 val_epoch_loss))
+        train_epochs_losses.append(train_epochs_loss)
+        val_epoch_losses.append(val_epoch_loss)
+
+        dict = model.state_dict()
+        dict["loss"] = train_epochs_losses
+        dict["val_loss"] = val_epoch_losses
+    return dict
